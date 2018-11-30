359b829ff0e20eafdcff71882a37151c971dd069
kalyani7 <email@email.com>
2018-11-30 11:10:11 +0530

    BankExaample

--- src/com/mybus/api/Teller.java
+++ src/com/mybus/api/Teller.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 
 public interface Teller {
     Account createAccount(int id, String name, long balance) throws IOException;
-    Account deposit(double amount, int accountId);
+    Account deposit(double amount, int accountId) throws IOException;
     double withdraw(double amount, int accountId) throws IOException, InsufficientBalanceException;
-    boolean transfer(int sourceAccountId, int destinationAccountId, double amount);
+    boolean transfer(int sourceAccountId, int destinationAccountId, double amount) throws IOException;
     double getBalance(int accountId) throws IOException;
 }
