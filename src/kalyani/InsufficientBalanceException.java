fba751258b3ccd143522c9b2ecd4bcb5de3597aa
kalyani7 <email@email.com>
2018-11-30 12:08:34 +0530

    BankExaample

---
+++ src/kalyani/InsufficientBalanceException.java
@@ -0,0 +1,8 @@
+package kalyani;
+
+public class InsufficientBalanceException extends Exception{
+    private String message;
+    public InsufficientBalanceException(String insufficient_funds_) {
+        this.message = insufficient_funds_;
+    }
+}
